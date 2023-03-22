fccda74399875cde4f091eab8e40a271becc4612
orlandoca <orlandoca647@gmail.com>
2023-03-22 01:20:56 -0300

    iniciando componente landing

---
+++ client/src/components/landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function landing() {
+  return (
+    <div className="landing-page">
+      landing
+      <header>
+        <h1>Alquiler de terrazas</h1>
+        <nav>
+          <link to="login"> Iniciar Sesion</link>
+          <link to="signup"> Registrarse</link>
+        </nav>
+      </header>
+    </div>
+  );
+}
