60e8fc47f5963521a10c1dc6a623f8130f1b2340
camj10 <camilajisel06@gmail.com>
2023-03-26 11:10:27 -0300

    pdf avances

---
+++ client/src/components/BookingPDF.jsx
@@ -0,0 +1,38 @@
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import {format} from 'date-fns'
+// format DEBE SER DESDE LA API-->Ver para adaptar al proyecto
+export default function BookingDetailComp(){
+    const generatePDF = tickets=>{
+    //INICIALIZO jsPDF
+    const doc= new jsPDF();
+    //Las columnas y sus títulos
+    const tableColumn=["name","phone","quantity","reason","date","start_time","send_time"];
+    const tableRows=[];
+    //Para cada ticket pasar toda su informacion dentro de un arreglo
+    tickets.forEach(ticket=>{
+        const ticketData=[
+            ticket.name,
+            ticket.phone,
+            ticket.quantity,
+            ticket.reason,
+            ticket.date,
+            ticket.start_time,
+            ticket.end_time,
+
+            format(new Date(ticket.update_at), "yyyy-MM-dd")
+        ];
+        //Poner cada informacion del ticket en una fila
+        table.Rows.push(ticketData);
+    });
+    //startY es como una especie de margin-top
+    doc.autoTable(tableColumn, tableRows,{startY:20});
+    const date= Date().split(" ");
+    //Utilizamos date string para generar el nombre de nuestro archivo
+    const dateStr= date[0]+date[1]+date[2]+date[3]+date[4]+date[5]+date[6];
+    //ticket title y margin top + margun left
+    doc.text("Reportes de reservas.",14,15);
+    //Definimos el nombre de nuestro archivo PDF
+    doc.save(`reporteReservas.pdf`);
+    }
+}
