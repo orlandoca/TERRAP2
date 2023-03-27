c857305e526d944254e477d5cbf3c73a112fdad3
orlandoca <orlandoca647@gmail.com>
2023-03-27 11:26:58 -0400

    feat: agregando un nuevo atributo building_name a booking

--- server/models/Booking_model.js
+++ server/models/Booking_model.js
@@ -3,11 +3,15 @@ const User = require("./User_model");
 const AutoPopulate = require("mongoose-autopopulate");
 
 const BookingSchema = new mongoose.Schema({
+  building_name: {
+    type: String,
+  },
   reason: {
     type: String,
     minlength: [3, "El mínimo es de 3 caracteres"],
     //required: [true, "Este dato es requerido"],
   },
+
   quantity: {
     type: Number,
     //minlength: [1, "El mínimo es de 1"],
