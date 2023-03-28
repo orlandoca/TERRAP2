9a40b197dd3190baac4a0cabfdb64665f326c2e6
orlandoca <orlandoca647@gmail.com>
2023-03-27 22:50:48 -0400

    fix: agregando atributos al reporte

--- client/src/components/BookingGeneratePDF.jsx
+++ client/src/components/BookingGeneratePDF.jsx
@@ -1,28 +1,36 @@
-import jsPDF from 'jspdf';
-import 'jspdf-autotable'; 
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 // import { format } from 'date-fns';
 
-const BookingGeneratePDF = booking => {
+const BookingGeneratePDF = (booking) => {
   const doc = new jsPDF();
-  const tableColumn = ["Cantidad de personas", "Fecha", "Nombre"];
+  const tableColumn = [
+    "Nombre del edificio",
+    "Nombre del inquilino",
+    "Cantidad de personas",
+    "Fecha",
+    "Horario Reserva",
+  ];
   const tableRows = [];
-  
-  booking.forEach(booking => {
+
+  booking.forEach((booking) => {
     const bookingData = [
+      booking.name_building,
+      booking.user.name,
       booking.quantity,
       booking.date,
-      booking.user.name
+      booking.start_time,
     ];
-    
+
     tableRows.push(bookingData);
   });
-  
+
   doc.autoTable(tableColumn, tableRows, { startY: 20 });
-  
+
   const date = new Date().toISOString().slice(0, 10);
   doc.text(`Reporte de reservas (${date})`, 14, 15);
-  
+
   doc.save(`ReporteReservas_${date}.pdf`);
-}
+};
 
 export default BookingGeneratePDF;
--- client/src/components/BookingReport.jsx
+++ client/src/components/BookingReport.jsx
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import BookingGeneratePDF from './BookingGeneratePDF';
+import BookingGeneratePDF from "./BookingGeneratePDF";
 
 const BookingReport = () => {
   const [booking, setBooking] = useState([]);
 
   useEffect(() => {
     const getAllBooking = async () => {
-      try { 
-        const response = await axios.get("http://localhost:8000/api/booking/getAll");
-        console.log("Para ver que tiene Booking: ",booking);
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/api/booking/getAll"
+        );
+        console.log("Para ver que tiene Booking: ", booking);
         setBooking(response.data);
       } catch (err) {
         console.log("error");
       }
     };
-    
+
     getAllBooking();
   }, [booking]);
   return (
     <div>
-          <button
-            className="btn btn-info ml-10"
-            onClick={() => BookingGeneratePDF(booking)}
-          >
-            Generar reporte
-          </button>
+      <button
+        className="btn btn-info ml-10"
+        onClick={() => BookingGeneratePDF(booking)}
+      >
+        Generar reporte
+      </button>
     </div>
   );
 };
